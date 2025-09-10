9aafe858bbe7e4ade92184c2b38f5bc19e15e0ef
Vagner Messias <vagnermessias.vmc@gmail.com>
2025-09-10 17:27:40 -0300

    adjust on VAO and shaders

--- VolumetricClouds/VolumetricClouds/shaderClass.h
+++ VolumetricClouds/VolumetricClouds/shaderClass.h
@@ -12,15 +12,17 @@ std::string get_file_contents(const char* filename);
 
 class Shader
 {
-public:
-	// Reference ID of the Shader Program
-	GLuint ID;
-	// Constructor that build the Shader Program from 2 different shaders
-	Shader(const char* vertexFile, const char* fragmentFile);
+	public:
+		// Reference ID of the Shader Program
+		GLuint ID;
+		// Constructor that build the Shader Program from 2 different shaders
+		Shader(const char* vertexFile, const char* fragmentFile);
 
-	// Activates the Shader Program
-	void Activate();
-	// Deletes the Shader Program
-	void Delete();
+		// Activates the Shader Program
+		void Activate();
+		// Deletes the Shader Program
+		void Delete();
+	private:
+		void compileErrors(unsigned int shader, const char* type);
 };
 #endif
